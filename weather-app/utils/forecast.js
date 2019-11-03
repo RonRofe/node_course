13705c07b6e37f5d4b55bf47b239c83ee502b75f
rapitec <ron@rapitec.co.il>
2019-11-03 15:16:38 +0200

    es6 objects on weather app

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -3,15 +3,15 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/41a3fe85b932f3fdfd3deeff3b61b2ec/' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude) + '?units=si'
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if(error) {
             callback('Unable to connect to weather service!', undefined)
-        } else if(response.body.error) {
+        } else if(body.error) {
             callback('Unable to find location!', undefined)
         } else {
             callback(undefined, {
-                temperature: response.body.currently.temperature,
-                precipProbability: response.body.currently.precipProbability
+                temperature: body.currently.temperature,
+                precipProbability: body.currently.precipProbability
             })
         }
     })
