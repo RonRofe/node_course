7e0ec2158490f66fa8568ecc1b33cce4424483e4
rapitec <ron@rapitec.co.il>
2019-11-03 14:50:25 +0200

    input

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,16 +3,23 @@ const request = require('request')
 const geocode = require('./utils/geocode.js')
 const forecast = require('./utils/forecast')
 
-geocode('Tel Aviv', (error, data) => {
-    if(error) {
-        return console.log(error)
-    }
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+const address = process.argv[2]
+
+if(!address) {
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, data) => {
         if(error) {
             return console.log(error)
         }
-
-        console.log(data.location)
-        console.log(forecastData)
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if(error) {
+                return console.log(error)
+            }
+    
+            console.log(data.location)
+            console.log(forecastData)
+        })
     })
-})
+    
+}
