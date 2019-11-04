2bd6fdc73f8db0ab06f3c3beaa5a73c0f2c69038
rapitec <ron@rapitec.co.il>
2019-11-04 10:22:03 +0200

    dynamic pages with templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,28 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Ron Rofe'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Ron Rofe'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'Help message is here'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
