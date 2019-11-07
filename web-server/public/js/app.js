6616212d78d0d1978d4571e166849cb1aa608934
rapitec <ron@rapitec.co.il>
2019-11-07 20:44:21 +0200

    adding input to insert location

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,12 @@
 console.log('Javascript is loaded!')
 
-fetch('http://puzzle.mead.io/puzzle').then(response => {
-    response.json().then(data => {
-        console.log(data)
-    })
-})
-
-fetch('http://localhost:3000/weather?address=!').then(response => {
+const weatherForm = document.querySelector('form')
+const searchElement = document.querySelector('input')
+weatherForm.addEventListener('submit', event => {
+    event.preventDefault()
+    const location = searchElement.value
+    
+    fetch('http://localhost:3000/weather?address='+location).then(response => {
     response.json().then(data => {
         if(data.error) {
             console.log(data.error)
@@ -15,4 +15,5 @@ fetch('http://localhost:3000/weather?address=!').then(response => {
             console.log(data.forecastData)
         }
     })
+})
 })
