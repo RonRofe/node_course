3911c401bfda4a218767fd0caf2a289f81f56d8f
rapitec <ron@rapitec.co.il>
2019-11-07 20:52:21 +0200

    activate inputs to fetch data

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,17 +2,24 @@ console.log('Javascript is loaded!')
 
 const weatherForm = document.querySelector('form')
 const searchElement = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
 weatherForm.addEventListener('submit', event => {
     event.preventDefault()
     const location = searchElement.value
     
+    messageOne.textContent = 'Loading'
+    messageTwo.textContent = ''
+
     fetch('http://localhost:3000/weather?address='+location).then(response => {
     response.json().then(data => {
         if(data.error) {
-            console.log(data.error)
+            messageOne.textContent = data.error
         } else {
-            console.log(data.location)
-            console.log(data.forecastData)
+            messageOne.textContent = data.location
+            messageTwo.textContent = JSON.stringify(data.forecastData)
         }
     })
 })
