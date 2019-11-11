2dc49c5e8a134eef0d81b635a8ab4ae3374f45ee
rapitec <ron@rapitec.co.il>
2019-11-11 20:55:42 +0200

    validation + add validtor package

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -7,23 +7,29 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true
     },
     age: {
-        type: Number
+        type: Number,
+        validate(value) {
+            if(value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
 })
 
-// const me = new User({
-//     name: 'Private',
-//     age: 'Mike'
-// })
+const me = new User({
+    name: 'Mike',
+    age: -1
+})
 
-// me.save().then(() => {
-//     console.log(me)
-// }).catch(error => {
-//     console.log('Error!', error)
-// })
+me.save().then(() => {
+    console.log(me)
+}).catch(error => {
+    console.log('Error!', error)
+})
 
 const Task = mongoose.model('Task', {
     description: {
@@ -34,13 +40,13 @@ const Task = mongoose.model('Task', {
     }
 })
 
-const task = new Task({
-    description: 'Learn the Mongoose library',
-    completed: false
-})
+// const task = new Task({
+//     description: 'Learn the Mongoose library',
+//     completed: false
+// })
 
-task.save().then(() => {
-    console.log(task)
-}).catch(error => {
-    console.log(error)
-})
+// task.save().then(() => {
+//     console.log(task)
+// }).catch(error => {
+//     console.log(error)
+// })
