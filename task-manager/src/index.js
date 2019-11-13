616baeb8f081c24a312cb75e1e20dbf5792f9e73
rapitec <ron@rapitec.co.il>
2019-11-13 20:25:28 +0200

    relationship playground

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -14,13 +14,17 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const pet = {
-    name: 'Hal'
+const Task = require('./models/task')
+const User = require('./models/user')
+
+const main = async () => {
+    // const task = await Task.findById('5dcc48957093723f04f3a937')
+    // await task.populate('owner').execPopulate()
+    // console.log(task.owner)
+
+    const user = await User.findById('5dcc48034d6bcb4b701be8fd')
+    await user.populate('tasks').execPopulate()
+    console.log(user.tasks)
 }
 
-pet.toJSON = function() {
-    console.log(this)
-    return this
-}
-
-console.log(JSON.stringify(pet))
+main()
