dad69428323b0c7b6d5e929344cf236715dd841d
rapitec <ron@rapitec.co.il>
2019-11-14 12:38:21 +0200

    file validation

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,7 +8,20 @@ const port = process.env.PORT || 3000
 
 const multer = require('multer')
 const upload = multer({
-    dest: 'images'
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        // callback(new Error('File must be a PDF'))
+        // callback(undefined, true)
+
+        if(!file.originalname.match(/\.(doc|docs)$/)) {
+            return cb(new Error('File must be a PDF'))
+        }
+
+        cb(undefined, true)
+    }
 })
 app.post('/upload', upload.single('upload'), (req, res) => {
     res.send()
