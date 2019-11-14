58a4925d051212229be697a8f09456c624ee10bb
rapitec <ron@rapitec.co.il>
2019-11-14 21:52:36 +0200

    cmd-env package installation + usage

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -1,7 +1,6 @@
 const sgMail = require('@sendgrid/mail')
-const sendgridAPIKey = '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'
 
-sgMail.setApiKey(sendgridAPIKey)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
