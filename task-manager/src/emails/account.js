e60d892d8a4d417e9c912c3a277c91334d88aa06
rapitec <ron@rapitec.co.il>
2019-11-14 21:04:48 +0200

    testing sendgrid email sending

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -1,5 +1,11 @@
 const sgMail = require('@sendgrid/mail')
 const sendgridAPIKey = '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'
 
+sgMail.setApiKey(sendgridAPIKey)
 
-
+sgMail.send({
+    to: 'ron@rapitec.co.il',
+    from: 'ron@rapitec.co.il',
+    subject: 'This is my first creation!',
+    text: 'I hope this one actually get to you.'
+})
