a5fcfc6d91c14d12df4e314a679d051d3d7572cd
rapitec <ron@rapitec.co.il>
2019-11-15 19:39:07 +0200

    async tests

--- task-manager/test/math.test.js
+++ task-manager/test/math.test.js
@@ -1,4 +1,7 @@
-const { calculateTip } = require('../src/math')
+const {
+    calculateTip,
+    add
+} = require('../src/math')
 
 test('Should calculate total with tip', () => {
     const total = calculateTip(10, .3)
@@ -8,4 +11,23 @@ test('Should calculate total with tip', () => {
 test('Should calculate total with default tip', () => {
     const total = calculateTip(10)
     expect(total).toBe(12.5)
+})
+
+// test('Async test demo', done => {
+//     setTimeout(() => {
+//         expect(1).toBe(2)
+//         done()
+//     }, 2000);
+// })
+
+test('Should add two numbers', done => {
+    add(2, 3).then(sum => {
+        expect(sum).toBe(5)
+        done()
+    })
+})
+
+test('Should add two number async/await', async () => {
+    const sum = await add(11, 22)
+    expect(sum).toBe(32)
 })
