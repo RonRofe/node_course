097ec5ec026bd44e187d48ecf5f93edb308bded8
rapitec <ron@rapitec.co.il>
2019-11-16 15:40:58 +0200

    first task test

--- task-manager/tests/task.test.js
+++ task-manager/tests/task.test.js
@@ -5,6 +5,15 @@ const { userOneId, userOne, setupDatabase } = require('./fixtures/db')
 
 beforeEach(setupDatabase)
 
-test('Should create task for a user', () => {
-
+test('Should create task for a user', async () => {
+    const response = await request(app)
+        .post('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            description: 'Frommy test'
+        })
+        .expect(201)
+    const task = await Task.findById(response.body._id)
+    expect(task).not.toBeNull()
+    expect(task.completed).toBe(false)
 })
